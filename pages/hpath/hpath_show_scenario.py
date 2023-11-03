1eb268fc11eae14f2135c8518b2dfe54a33e3d65
Yin-Chi Chan <ycc39@cam.ac.uk>
2023-11-03 16:24:30 +0000

    Update hpath_show_scenario.py

--- pages/hpath/hpath_show_scenario.py
+++ pages/hpath/hpath_show_scenario.py
@@ -1,6 +1,9 @@
 """Page for showing results for a single simulation scenario."""
 import dash
-from dash import html
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+
+from pages import templates
 
 dash.register_page(
     __name__,
@@ -13,8 +16,29 @@ def layout(scenario_id=None):
     """Build the layout to show the given scenario's result.
     """
     # TODO fetch result, if error show error message
+    result = None
 
     # TODO replicate Lakee's result page?
-    return html.Div(
-        f"The user requested scenario ID: {scenario_id}."
+    return dbc.Stack(
+        [
+            templates.breadcrumb(
+                ['Home', 'Histopathology: Simulator', 'Scenarios', f'{scenario_id}'],
+                ['hpath', 'view', 'single']
+            ),
+            templates.page_title('Histopathology: Single-Scenario Results'),
+
+            (  # ternary statement
+                html.Div(
+                    [
+                        html.B('Error: '),
+                        html.Span(f"Could not fetch scenario with ID: {scenario_id}")
+                    ],
+                    style={'color': '#a00'}
+                ) if result is None else
+                html.H2(
+                    result.sc_name,
+                    style={'font-size': '2.2rem'}
+                )
+            )  # end ternary statement
+        ]
     )
