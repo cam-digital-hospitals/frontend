5ec665686d5f83d068a673a21cea25638cfb6a8d
Yin-Chi Chan <ycc39@cam.ac.uk>
2023-11-04 16:57:34 +0000

    Copy datatype defs from hpath-sim

---
+++ chart_datatypes.py
@@ -0,0 +1,45 @@
+"""Datatypes for chart data."""
+from dataclasses import dataclass, field
+import pandas as pd
+
+
+@dataclass
+class ChartData:
+    """Jsonifiable chart data representation for a single data series."""
+    x: list[float | str]
+    y: list[float]
+    ymin: list[float] | None = field(default=None, kw_only=True)
+    ymax: list[float] | None = field(default=None, kw_only=True)
+
+    @staticmethod
+    def from_pandas(obj: pd.DataFrame | pd.Series) -> 'ChartData':
+        """Instantiate a ChartData object from a pandas :py:class:`~pandas.DataFrame`
+        or :py:class:`~pandas.Series`."""
+        series = obj.iloc[:, 0] if isinstance(obj, pd.DataFrame) else obj
+        return __class__(x=series.index.tolist(), y=series.values.tolist())
+
+
+@dataclass
+class MultiChartData:
+    """Jsonifiable chart data representation for multiple data series.
+
+    **Note**: only line charts are supported currently for this data type,
+    thus ``x`` must be numeric, unlike for :py:class:`ChartData` which
+    can also represent bar chart data with ``string`` x values.
+    """
+    x: list[float]
+    y: list[list[float]]
+    """List of line series.  Each series is a ``list[float]``."""
+    labels: list[str] = field(kw_only=True)
+    """Legend labels for each line series."""
+    ymin: list[list[float]] | None = field(default=None, kw_only=True)
+    ymax: list[list[float]] | None = field(default=None, kw_only=True)
+
+    @staticmethod
+    def from_pandas(df: pd.DataFrame) -> 'MultiChartData':
+        """Instantiate a MultiChartData object from a pandas :py:class:`~pandas.DataFrame`."""
+        return __class__(
+            df.index.tolist(),
+            df.T.values.tolist(),
+            labels=df.columns.tolist()
+        )
---
+++ kpis.py
@@ -0,0 +1,47 @@
+"""Compute KPIs for a model from simulation results.
+
+Keep this module synced with kpis.py in `hpath-sim`.
+"""
+from dataclasses import dataclass, field
+from typing import TypedDict
+
+from chart_datatypes import ChartData, MultiChartData
+
+
+Progress = TypedDict('Progress', {
+    '7': float,
+    '10': float,
+    '12': float,
+    '21': float
+})
+"""Returns the proportion of specimens completed within 7, 10, 12, and 21 days."""
+
+LabProgress = TypedDict('LabProgress', {
+    '3': float
+})
+"""Returns the proportion of specimens with lab component completed within three days."""
+
+
+@dataclass(kw_only=True)
+class Report:
+    """Dataclass for reporting a set of KPIs for passing to a frontend visualisation server.
+    In the current implementation, this is https://github.com/lakeesiv/digital-twin"""
+    overall_tat: float
+    lab_tat: float
+    progress: Progress
+    lab_progress: Progress
+    tat_by_stage: ChartData
+    resource_allocation: dict[str, ChartData]  # ChartData for each resource
+    wip_by_stage: MultiChartData
+    utilization_by_resource: ChartData
+    q_length_by_resource: ChartData
+    hourly_utilization_by_resource: MultiChartData
+
+    overall_tat_min: float | None = field(default=None)
+    overall_tat_max: float | None = field(default=None)
+    lab_tat_min: float | None = field(default=None)
+    lab_tat_max: float | None = field(default=None)
+    progress_min: Progress | None = field(default=None)
+    progress_max: Progress | None = field(default=None)
+    lab_progress_min: LabProgress | None = field(default=None)
+    lab_progress_max: LabProgress | None = field(default=None)
